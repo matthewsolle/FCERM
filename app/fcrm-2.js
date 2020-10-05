7a5bb698d8588460c6bb61be986a6a1483ec4a0f
Matthew Solle <matthewsolle@gmail.com>
2020-10-05 14:11:32 +0100

    added two new iterations

---
+++ app/fcrm-2.js
@@ -0,0 +1,61 @@
+const express = require('express')
+const { get } = require('browser-sync')
+const router = express.Router()
+const fs = require('fs')
+
+var folder = "fcrm-2"
+var versionServiceName = "Flood and coastal erosion risk management"
+router.use(function (req, res, next) {
+  // store in locals this can then be used in pages as {{folder}} etc
+  res.locals.folder=folder
+  res.locals.versionServiceName=versionServiceName
+  next()
+})
+
+var searchColumn = 'title'
+
+
+// Routes
+
+// Search
+// Variant Specialist publisher
+router.get('/search-results_spec_pub-default', function(req, res) {
+    res.render(folder + '/search-results_spec_pub-default', { results: filterReports(req.query.name, req.query.topics,req.query.themes,req.query.status), url: req.url  })
+})
+
+
+// Others
+router.get('/product-details-default/:reportNumber', function(req, res) {
+    res.render(folder + '/product-details-default', findReport(req.params.reportNumber))
+})
+
+
+// Functions
+function findReport(reportNumber) {
+    return getReportData('reports').find(element => element.id === reportNumber)
+}
+
+function filterReports(searchText,topics,themes,status) {
+    searchText = searchText.toLowerCase()
+    let reportData = getReportData('reports')
+    if (searchText) {
+        reportData = reportData.filter(element => element[searchColumn].toLowerCase().includes(searchText,-1))
+    }
+        if (topics) {
+        reportData = reportData.filter(element => element.topics === topics)
+    }
+    if (themes != '_unchecked') {
+        reportData = reportData.filter(element => themes.includes(element.themes))
+    }
+    if (status != '_unchecked') {
+        reportData = reportData.filter(element => status.includes(element.status))
+    }
+
+    return reportData
+}
+
+function getReportData(reportData) {
+    return require('./views/' + folder + '/data/reports/reports.json')
+}
+
+module.exports = router
--- app/routes.js
+++ app/routes.js
@@ -9,5 +9,7 @@ router.use('/v1', require('./routes_v1'))
 router.use('/v2', require('./routes_v2'))
 router.use('/v3', require('./routes_v3'))
 router.use('/fcrm-1', require('./fcrm-1'))
+router.use('/fcrm-2', require('./fcrm-2'))
+router.use('/fcrm-3', require('./fcrm-3'))
 
 module.exports = router
