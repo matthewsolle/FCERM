4603cf74e782bf8c7effd55aed9592c35d49509d
Matthew Solle <matthewsolle@gmail.com>
2020-10-06 11:51:49 +0100

    update to service name

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Flood and coastal erosion risk management reports',
+  serviceName: 'Flood and coastal erosion risk management',
 
   // Default port that prototype runs on
   port: '3000',
